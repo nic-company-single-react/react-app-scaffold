304de9b712ee98666ec288a9a5393fa8119704d7
redsky0212 <wihyun0238@gmail.com>
2026-04-17 16:14:22 +0900

    현시점 빌드 성공

--- src/domains/main/pages/MainIndex.tsx
+++ src/domains/main/pages/MainIndex.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/shared/components/shadcn/components/ui/button';
+
 export default function MainIndex(): React.ReactNode {
 	return (
 		<div>
 			<h1>Main Index</h1>
+			<Button onClick={() => alert('Button clicked')}>Click me</Button>
 		</div>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			'@': resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		base: env.VITE_BASE_URL,
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				'@': resolve(__dirname, './src'),
+				'@app-types': resolve(__dirname, './src/types'),
+			},
 		},
-	},
-	server: {
-		port: 5173,
-	},
+		server: {
+			port: Number(env.PORT) || 5173,
+		},
+	};
 });
