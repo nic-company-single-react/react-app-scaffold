853914c64e4ce8d4056d83e78f0d7a4119eb0dc1
redsky0212 <wihyun0238@gmail.com>
2026-04-17 11:11:13 +0900

    전역 타입 선언 폴더 추가

---
+++ src/types/global.d.ts
@@ -0,0 +1,22 @@
+//import type { IRouter } from '@/types/router';
+//import type { IUtils } from '@/types/common';
+//import type { IUI } from '@/types/components';
+
+declare global {
+	interface Window {
+		//$router: IRouter;
+		//$util: IUtils;
+		//$ui: IUI;
+	}
+
+	//const $router: IRouter;
+	//const $util: IUtils;
+	//const $ui: IUI;
+}
+
+/**
+ * export {}가 반드시 필요한 이유:
+ * .d.ts 파일이라도 import/export 구문이 하나도 없으면 TypeScript는 이 파일을 script(전역 파일)로 간주합니다. 그 상태에서 declare global { } 을 쓰면 오류가 납니다. export {}를 추가해야 TypeScript가 이 파일을 module로 인식하고, 그 안의 declare global이 비로소 올바른 전역 augmentation으로 작동합니다.
+ * 그런데 만약 import 문이 하나라도 있으면 export {} 구문은 필요없음.
+ */
+export {};
