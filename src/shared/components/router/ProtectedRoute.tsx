1e4c9bc56a49923ffe49bf0bbe2211a687eda600
redsky0212 <wihyun0238@gmail.com>
2026-06-19 17:44:17 +0900

    라우트 인증관련 컴포넌트 추가

---
+++ src/shared/components/router/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+// access token 체크를 통해 라우터 이동전 로그인 상태 체크
+import { Navigate, Outlet } from 'react-router';
+
+// 라우터 페이지 이동 시 token 인증처리 컴포넌트
+// token 인증처리 관련 로직은 상황에 따라 변경하여 사용합니다.
+const ProtectedRoute = () => {
+	const token = localStorage.getItem('access_token');
+	return token ? (
+		<Outlet />
+	) : (
+		<Navigate
+			to="/auth/login"
+			replace
+		/>
+	);
+};
+
+export default ProtectedRoute;
